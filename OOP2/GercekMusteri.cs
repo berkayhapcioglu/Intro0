023a69a71f3687adeef86e3b521f50ff913fda7a
berkayhapcioglu <berkaygamess@gmail.com>
2021-07-25 23:06:24 +0300

    25.07.2021

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //Individual
+    //Inheritance
+    class GercekMusteri:Musteri
+    {
+        public string TcNo { get; set; }
+        public string Ad { get; set; }
+        public string Soyadi { get; set; }
+        
+    }
+}
